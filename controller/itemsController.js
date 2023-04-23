fff7c282e2513040b5fe20438f75829ebb2810f0
rujroot <ruj9root@gmail.com>
2023-04-23 15:19:37 +0700

    eiei

--- controller/itemsController.js
+++ controller/itemsController.js
@@ -10,9 +10,26 @@ const {
 
 const docClient = new DynamoDBClient({ regions: process.env.AWS_REGION });
 
-exports.getGroupMembers = async (req, res) => {
+// exports.getGroupMembers = async (req, res) => {
+//   const params = {
+//     TableName: process.env.aws_group_members_table_name,
+//   };
+//   try {
+
+//     const data = await docClient.send(new ScanCommand(params));
+//     console.log(data);
+//     res.send(data.Items);//response 
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send(err);
+//   }
+// };
+
+// TODO #1.1: Get items from DynamoDB
+exports.getItems = async (req, res) => {
+  // You should change the response below.
   const params = {
-    TableName: process.env.aws_group_members_table_name,
+    TableName: process.env.aws_items_table_name, //see env
   };
   try {
     const data = await docClient.send(new ScanCommand(params));
@@ -23,26 +40,50 @@ exports.getGroupMembers = async (req, res) => {
   }
 };
 
-// TODO #1.1: Get items from DynamoDB
-exports.getItems = async (req, res) => {
-  // You should change the response below.
-  res.send("This route should get all items in DynamoDB.");
-};
-
 // TODO #1.2: Add an item to DynamoDB
 exports.addItem = async (req, res) => {
   const item_id = uuidv4();
   const created_date = Date.now();
-  const item = { item_id: item_id, ...req.body, created_date: created_date };
+  const item = { id: item_id, ...req.body, created_date: created_date };
+
+  //console.log(item);
 
   // You should change the response below.
-  res.send("This route should add an item in DynamoDB.");
+  const params = {
+    TableName: process.env.aws_items_table_name,
+    Item: item,
+  };//see body in postman
+  
+  try {
+    console.log("eiei", params);
+    const data = await docClient.send(new PutCommand(params)); //PutCommand
+    res.send(data);
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 };
 
 // TODO #1.3: Delete an item from DynamDB
 exports.deleteItem = async (req, res) => {
-  const item_id = req.params.item_id;
+  const id = req.params.item_id;
 
   // You should change the response below.
-  res.send("This route should delete an item in DynamoDB with item_id.");
+  
+  const params = {
+    TableName: process.env.aws_items_table_name,
+    Key: {
+      id,
+    },
+  };
+ 
+ 
+  try {
+    const data = await docClient.send(new DeleteCommand(params));
+    res.send(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 };
--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -4,7 +4,7 @@ const itemsController = require("../controller/itemsController");
 const router = express.Router();
 
 router.get("/", itemsController.getItems);
-router.get("/members", itemsController.getGroupMembers);
+// router.get("/members", itemsController.getGroupMembers);
 router.post("/", itemsController.addItem);
 router.delete("/:item_id", itemsController.deleteItem);
 
